2c3a1e2b25895222aad773d2a592744d5a8e5736
Jeff Gibson <jeffgibson@agcode.com>
2019-11-14 08:39:06 -0600

    Initial code push, entire project

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseKeys = {
+  apiKey: 'AIzaSyA7lGAIiHw72a30LkMw7jWOsa5FfKPq_ZM',
+  authDomain: 'gibsonia-cc.firebaseapp.com',
+  databaseURL: 'https://gibsonia-cc.firebaseio.com',
+  projectId: 'gibsonia-cc',
+  storageBucket: 'gibsonia-cc.appspot.com',
+  messagingSenderId: '1066716669996',
+  appId: '1:1066716669996:web:026a4283fea1543b65c989'
+}
